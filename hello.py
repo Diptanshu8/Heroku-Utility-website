ea0fb9e0898c6da61bb07442a1391382bd30ca4c
Diptanshu8 <diptanshuj@gmail.com>
2015-10-23 05:16:20 +0530

    base template added

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import request
-
+from flask import render_template
 import os
+
 app = Flask(__name__)
 #app.config.from_pyfile('config.py')
 
 @app.route('/self',methods=['GET','POST'])
 def hello():
-	return str(request.data)
+	ip= str(request.data)
+	return render_template('base.html',title='DJ-pi',ip=ip)	
 if __name__=="__main__":
         app.run(host = '0.0.0.0')
