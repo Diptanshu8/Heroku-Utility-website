345668112a3c78d4a1d3240ed2e3b9928759fbf7
Diptanshu8 <diptanshuj@gmail.com>
2015-10-23 05:34:39 +0530

    new_2

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask import request
-from flask import render_template
 import os
 
 app = Flask(__name__)
 #app.config.from_pyfile('config.py')
 
-@app.route('/self',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def hello():
 	ip= str(request.data)
-	return render_template('base.html',title='DJ-pi',ip=ip)	
+	return ip	
 if __name__=="__main__":
         app.run(host = '0.0.0.0')
