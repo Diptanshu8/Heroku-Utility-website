1477376a6765d18d2d9e19622a876723f517b1b1
Diptanshu8 <diptanshuj@gmail.com>
2015-10-23 05:37:19 +0530

    .

--- hello.py
+++ hello.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def hello():
-	ip= str(request.data)
-	return ip	
+	return str(request.data)	
 if __name__=="__main__":
         app.run(host = '0.0.0.0')
