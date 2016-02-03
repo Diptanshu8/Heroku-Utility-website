6e7d829d4276fccc817e9f697c19645298ddab70
Diptanshu8 <diptanshuj@gmail.com>
2016-02-04 01:03:58 +0530

    changes are made for showing timetable

--- hello.py
+++ hello.py
@@ -21,38 +21,9 @@ def ipdisplay():
 	f.close()
 	return ip
 
-@app.route('/save_link',methods=['GET','POST'])
-def linkdatabaseupdate():
-	with open("links.csv",'a'):
-		w = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		w.writerow(str(request.data))
-	return request.data
-
-@app.route('/show_link',methods=['GET'])
-def linkdatabaseshow():
-	f=open("links.csv",'r')
-	links = f.read()
-	f.close()
-	return links
-
-def data_write(timestamp,link,description):
-	with open('data.csv','a') as f:
-		fieldnames = ['Time-Stamp','Link','Description']
-		writer = csv.DictWriter(f,fieldnames=fieldnames)
-		if link == ' ' and timestamp == ' ' and description == ' ': 
-			writer.writeheader()
-		elif not link == ' ' or not link =='':
-			writer.writerow({'Time-Stamp':timestamp,'Link':link,'Description':description})
-		else:
-@app.route('/temp',methods=['GET'])
+@app.route('/tt',methods=['GET'])
 def temp():
-	f=open('ip.txt','r')
-        ip=f.read()
-        f.close()
-	user={"ip":ip}
-	date=ip.split("IST")
-	user["date"]=date
-	return render_template('base.html',user=user)
+	return render_template('base.html')
 
 if __name__=="__main__":
         app.run(host = '0.0.0.0')
