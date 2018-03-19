71affb9bbef91e6ad6b51a741dfd2999b37292f7
Diptanshu8 <diptanshuj@gmail.com>
2018-03-19 12:07:48 +0530

    removed config.py as it just added a DEBUG = TRUE message to the server. added the word_of_the_day to the server and made corresponding changes.
    FW: make the home page beautiful and add redirection links to the following tt and word of the day pages.
        make the word of the day info attractive.

--- hello.py
+++ hello.py
@@ -3,29 +3,36 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import os
+import sys
+import word_of_the_day.word as word
 
 app = Flask(__name__)
-#app.config.from_pyfile('config.py')
 
+#function to save the IP address of my raspi
 @app.route('/save_ip',methods=['GET','POST'])
 def databaseupdate():
-	#user = {'ip': 'str(request.form)'}
 	f=open("ip.txt",'w')
 	f.write(str(request.data))
 	f.close()
 	return request.data
+
+#function to retrieve the stored IP address of my raspi
 @app.route('/show_ip',methods=['GET'])
 def ipdisplay():
 	f=open('ip.txt','r')
 	ip=f.read()
 	f.close()
 	return ip
-@app.route('/pwd')
-def tp():
-	return os.getcwd()
+
+#function to show the timetable that is stored on the server.
 @app.route('/tt',methods=['GET'])
-def temp():
+def timetable_display():
 	return render_template('base.html')
+
+#function to show the word of the day, its meaning and citations.
+@app.route('/word', methods=['GET'])
+def word_of_the_day_to_server():
+    return word.find_word_of_the_day()
 
 if __name__=="__main__":
         app.run(host = '0.0.0.0')
